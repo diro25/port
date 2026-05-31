68550a7041ff4deeef750e076abd508b0b1d47b1
diriba <diribaaadunyaa@gmail.com>
2026-05-31 11:03:36 +0300

    functionis the best auqer that helps to join wannt you want in the or in your work

---
+++ practice.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import keyword
+x=keyword.kwlist
+for i in x:
+    print(i)
---
+++ that.py
@@ -0,0 +1,11 @@
+def that():  
+  print("That is a function called 'that'.")
+  if 6.9 > 59.9:
+    print("This is inside the if statement.") 
+  else:
+    print("This is inside the else statement.")
+    for i in range(5):
+      print(f"This is inside the for loop: {i}")
+  print("This is the end of the function.")
+  
+that()
